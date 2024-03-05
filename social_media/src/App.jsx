b76fe127bcfbf0603fa0441c6ba02b89a9625b22
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-03-05 11:15:49 +0500

    Add Sidebar

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -2,13 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
+import Sidebar from "./Components/Sidebar"
 
 function App() {
 
   return (
     <>
+     <Sidebar></Sidebar>
      <Header></Header>
+     
      <Footer></Footer>
+     
     </>
   )
 }
--- social_media/src/Components/Header.jsx
+++ social_media/src/Components/Header.jsx
@@ -1,13 +1,13 @@
 const Header=()=>{
   return <>
-  <header class="p-3 text-bg-dark">
+  <header className="p-3 text-bg-dark">
     <div className="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"></use></svg>
         </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
           <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
           <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
