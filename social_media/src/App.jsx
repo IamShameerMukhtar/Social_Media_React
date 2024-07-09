1f1f6188f389f3a282a35e62a07b19039f147447
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-07-09 13:13:18 +0500

    Create UI

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -4,18 +4,29 @@ import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Sidebar from "./Components/Sidebar"
 import CreatePost from "./Components/CreatePost"
-
+import PostList from "./Components/PostList"
+import { useState } from "react"
 function App() {
+const [selectedTab,setSelectedTab]=useState("Home");
 
-  return (
+
+
+
+
+return (
     <>
     <div className="app-container">
-     <Sidebar></Sidebar>
+     <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></Sidebar>
      
     <div className="content"><Header></Header>
-    <CreatePost></CreatePost>
-     <Footer></Footer></div>
+    {selectedTab==="Home"?<PostList></PostList>:<CreatePost></CreatePost> }
+    
+    
      </div>
+     
+     </div>
+     
+     <Footer></Footer>
     </>
   )
 }
--- social_media/src/Components/Sidebar.jsx
+++ social_media/src/Components/Sidebar.jsx
@@ -1,21 +1,31 @@
-const Sidebar=()=>{
-return <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style={{width: "280px"}}>
+const Sidebar=({selectedTab ,setSelectedTab})=>{
+
+const handleTab=(value)=>{
+ setSelectedTab(value);
+}
+
+
+
+
+return <>
+<div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style={{width: "280px"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
       <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
       <span className="fs-4">Sidebar</span>
     </a>
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
+      <li onClick={()=>{handleTab("Home")}} className="nav-item">
+        <a href="#" className={`nav-link ${selectedTab==='Home' && "active"} `} aria-current="page">
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
           Home
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link link-body-emphasis">
+      <li onClick={()=>{handleTab("CreatePost")}}>
+        <a href="#" className={`nav-link link-body-emphasis ${selectedTab==="CreatePost" && "active"}`}>
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
-          Create Post
+        Create Post
+          
         </a>
       </li>
       
@@ -35,6 +45,14 @@ return <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sid
       </ul>
     </div>
   </div>
+
+  </>
+
+
+
+
+
+
 
 
 
